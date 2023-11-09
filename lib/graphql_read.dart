1e677130cc4e4610453fc004dc5b1287f5dad228
Hugo Bedford <36144175+hbedford@users.noreply.github.com>
2023-11-09 17:02:44 -0300

    ajust on converter string

--- lib/graphql_read.dart
+++ lib/graphql_read.dart
@@ -1,6 +1,6 @@
 import 'package:manager_api/requests/graphql_request.dart';
 import 'package:flutter/services.dart';
-import 'package:manager_api/utils/extensions.dart';
+import 'package:manager_api/utils/string_converter.dart';
 
 class GraphQLRead {
   GraphQLRead._();
@@ -17,7 +17,7 @@ class GraphQLRead {
     int quantityClose = 0;
 
     String fileResult = (await rootBundle.loadString(
-        'lib/src/services/graphql/${path.camelCaseToSnakeCase()}.graphql'));
+        'lib/src/services/graphql/${StringConverter.camelCaseToSnakeCase(path)}.graphql'));
 
     String stringType = type == RequestGraphQLType.query ? "query" : "mutation";
     bool firstLine = false;
---
+++ lib/utils/string_converter.dart
@@ -0,0 +1,15 @@
+class StringConverter {
+  StringConverter._();
+
+  static snakeCaseToCamelCase(String snakeCase) {
+    String camelCase = snakeCase.replaceAllMapped(
+        RegExp(r'([a-zA-Z])'), (match) => match.group(1)!.toUpperCase());
+    return camelCase[0].toUpperCase() + camelCase.substring(1);
+  }
+
+  static camelCaseToSnakeCase(String camelCase) {
+    String snakeCase = camelCase.replaceAllMapped(
+        RegExp(r'(?<!^)[A-Z]'), (match) => match.group(0)!.toLowerCase());
+    return snakeCase.toLowerCase();
+  }
+}
