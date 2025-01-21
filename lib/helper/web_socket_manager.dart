1b1862534281dc29e585c92cebec15a8973157d5
Hugo Bedford <36144175+hbedford@users.noreply.github.com>
2025-01-21 17:22:52 -0300

    changed version

---
+++ lib/helper/web_socket_manager.dart
@@ -0,0 +1,29 @@
+import 'package:manager_api/helper/web_socket_service.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+abstract class WebSocketManager {
+  late BehaviorSubject<dynamic> stream;
+  WebSocketType get socketType;
+
+  WebSocketChannel? get controller;
+
+  WebSocketManager({BehaviorSubject<dynamic>? stream});
+
+  Future<bool> initialize({
+    required String url,
+    required String token,
+    bool enablePing = true,
+  });
+
+  void checkConnection();
+  void closeSection();
+  Future<bool> create({
+    required String url,
+    required String token,
+    bool enablePing = true,
+  });
+  void setSocketType(WebSocketType value);
+  void debugger(String name);
+  void dispose();
+}
