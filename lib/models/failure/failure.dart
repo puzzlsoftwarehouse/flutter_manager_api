822279d4f55e0732747cfd8c7427f8c2b996b4e4
Hugo Bedford <hugo.bedford@gmail.com>
2025-07-24 16:18:34 -0300

    fix

--- lib/models/failure/failure.dart
+++ lib/models/failure/failure.dart
@@ -3,12 +3,14 @@ class Failure {
   final String title;
   final String message;
   final String? _log;
+  final String? error;
 
   Failure({
     required this.code,
     String? title,
     String? message,
     String? log,
+    this.error,
   })  : _log = log ?? '[$code] $message',
         title = title ?? 'Error!',
         message = message ?? "Unknown error, contact our support";
@@ -20,11 +22,13 @@ class Failure {
     String? title,
     String? message,
     String? log,
+    String? error,
   }) =>
       Failure(
         code: code ?? this.code,
         title: title ?? this.title,
         message: message ?? this.message,
         log: log ?? this.log,
+        error: error ?? this.error,
       );
 }
