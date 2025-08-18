16299151d4a8e1b58ab19262768475d7fc4a326f
DexterZP <48168094+DexterZP@users.noreply.github.com>
2025-08-18 19:05:00 -0300

    feat(manager_api): socket (#31)

--- lib/helper/web_socket_manager.dart
+++ lib/helper/web_socket_manager.dart
@@ -1,29 +1,27 @@
 import 'package:manager_api/helper/web_socket_service.dart';
 import 'package:rxdart/rxdart.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_client/web_socket_client.dart';
 
 abstract class WebSocketManager {
   late BehaviorSubject<dynamic> stream;
   WebSocketType get socketType;
 
-  WebSocketChannel? get controller;
+  WebSocket? get controller;
 
   WebSocketManager({BehaviorSubject<dynamic>? stream});
 
   Future<bool> initialize({
     required String url,
     required String token,
-    bool enablePing = true,
   });
 
   void sendMessage(String message);
 
-  void checkConnection();
+  void checkConnection(ConnectionState state);
   void closeSection();
   Future<bool> create({
     required String url,
     required String token,
-    bool enablePing = true,
   });
   void setSocketType(WebSocketType value);
   void debugger(String name);
--- lib/helper/web_socket_service.dart
+++ lib/helper/web_socket_service.dart
@@ -1,34 +1,25 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:developer';
 
 import 'package:flutter/foundation.dart';
 import 'package:manager_api/helper/web_socket_manager.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:web_socket_client/web_socket_client.dart';
 
 enum WebSocketType { connected, disconnected, trying }
 
 class WebSocketService extends WebSocketManager with ChangeNotifier {
   bool _isClosed = false;
-  bool enablePing = true;
 
-  WebSocketChannel? _controller;
+  WebSocket? _controller;
   @override
-  WebSocketChannel? get controller => _controller;
+  WebSocket? get controller => _controller;
 
   String? _url;
-  String? _token;
-
-  Timer? _timer;
-
-  bool _pong = false;
 
   WebSocketType _socketType = WebSocketType.trying;
   @override
   WebSocketType get socketType => _socketType;
-
-  bool _needReconnect = false;
 
   WebSocketService({BehaviorSubject<dynamic>? stream}) {
     super.stream = stream ?? BehaviorSubject();
@@ -38,13 +29,8 @@ class WebSocketService extends WebSocketManager with ChangeNotifier {
   Future<bool> initialize({
     required String url,
     required String token,
-    bool enablePing = true,
   }) async {
-    this.enablePing = enablePing;
-
     _url = url;
-    _token = token;
-
     if (_isClosed) return false;
 
     setSocketType(WebSocketType.trying);
@@ -52,22 +38,19 @@ class WebSocketService extends WebSocketManager with ChangeNotifier {
     try {
       String beforeString = url.contains("?") ? "&" : "?";
 
-      _controller?.sink.close();
+      _controller?.close();
       _controller = null;
-      _controller = WebSocketChannel.connect(
-          Uri.parse("$url${beforeString}token=$token"));
+      _controller = WebSocket(
+        Uri.parse("$url${beforeString}token=$token"),
+        backoff: ConstantBackoff(Duration(seconds: 5)),
+        pingInterval: Duration(seconds: 5),
+      );
 
-      checkConnection();
+      _controller!.connection.listen((state) => checkConnection(state));
 
-      _controller!.stream.listen((event) {
-        if (jsonDecode(event)['type'] == "pong") {
-          _pong = true;
-          if (_needReconnect) {
-            _needReconnect = false;
-            stream.add("reconnected");
-          }
-          return;
-        }
+      _controller!.messages.listen((event) {
+        debugger("Recebendo dados do WebSocket: $event");
+
         stream.add(event);
         setSocketType(WebSocketType.connected);
       }, onDone: () {
@@ -78,58 +61,44 @@ class WebSocketService extends WebSocketManager with ChangeNotifier {
       debugger("WebSocket Error: $e");
       stream.add("disconnected");
       setSocketType(WebSocketType.disconnected);
-      checkConnection();
       return false;
     }
 
-    debugger("WebSocket Connected: ${_controller != null}");
-    stream.add("connected");
-    setSocketType(WebSocketType.connected);
     return _controller != null;
   }
 
   @override
-  void checkConnection() {
-    if (!enablePing) return;
+  void checkConnection(ConnectionState state) {
     if (_controller == null || _isClosed) return;
 
-    _timer?.cancel();
-    _timer = Timer.periodic(Duration(seconds: 10), (timer) {
-      if (_controller == null) {
-        _cancelTimer();
-        return;
-      }
-      if (!enablePing) {
-        _cancelTimer();
-        return;
-      }
+    if (state is Connected) {
+      debugger("WebSocket Connected");
+      stream.add("connected");
+      setSocketType(WebSocketType.connected);
+    }
 
-      _controller!.sink.add(jsonEncode({"type": "ping"}));
-      _pong = false;
+    if (state is Reconnecting) {
+      debugger("WebSocket Reconnecting...");
+      setSocketType(WebSocketType.trying);
+    }
 
-      Future.delayed(Duration(seconds: 5), () {
-        if (!_pong && !_isClosed) {
-          if (_url == null || _token == null) return;
-          debugger("WebSocket Disconnected  Don`t have pong");
-          stream.add("disconnected");
-          setSocketType(WebSocketType.disconnected);
+    if (state is Disconnected) {
+      debugger("WebSocket Disconnected");
+      stream.add("disconnected");
+      setSocketType(WebSocketType.disconnected);
+    }
 
-          _cancelTimer();
-          _needReconnect = true;
-          initialize(url: _url!, token: _token!);
-          return;
-        }
-        if (_isClosed) {
-          _cancelTimer();
-        }
-      });
-    });
+    if (state is Reconnected) {
+      debugger("WebSocket Reconnected");
+      stream.add("connected");
+      setSocketType(WebSocketType.connected);
+    }
   }
 
   @override
   void sendMessage(String message) {
     if (_controller == null || _isClosed) return;
-    _controller!.sink.add(message);
+    _controller!.send(message);
   }
 
   @override
@@ -138,27 +107,19 @@ class WebSocketService extends WebSocketManager with ChangeNotifier {
     stream.add("disconnected");
     setSocketType(WebSocketType.disconnected);
 
-    _controller?.sink.close();
+    _controller?.close();
     _isClosed = true;
-    _cancelTimer();
     _controller = null;
-  }
-
-  void _cancelTimer() {
-    _timer?.cancel();
-    _timer = null;
   }
 
   @override
   Future<bool> create({
     required String url,
     required String token,
-    bool enablePing = true,
   }) async {
     bool success = await initialize(
       url: url,
       token: token,
-      enablePing: enablePing,
     );
 
     return success;
@@ -177,7 +138,6 @@ class WebSocketService extends WebSocketManager with ChangeNotifier {
 
   @override
   void dispose() {
-    _timer?.cancel();
     closeSection();
     super.dispose();
   }
