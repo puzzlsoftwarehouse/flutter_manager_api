98943119550721382f287b5a5339a7cbbf4b3958
Dexter <andredexter@live.com>
2025-08-18 10:32:34 -0300

    ...

--- lib/helper/send_media_web/send_media_web.dart
+++ lib/helper/send_media_web/send_media_web.dart
@@ -1,12 +1,8 @@
 import 'dart:async';
-import 'dart:convert';
-import 'dart:typed_data';
 import 'package:cross_file/cross_file.dart';
 import 'package:dio/dio.dart';
-import 'package:js_interop_utils/js_interop_utils.dart';
-import 'package:manager_api/helper/send_media_web/large_file_uploader.dart';
+import 'package:manager_api/helper/send_media_web/chunk.dart';
 import 'package:rxdart/subjects.dart';
-import 'package:web/web.dart' as html;
 
 class SendMediaWeb {
   SendMediaWeb._();
@@ -22,50 +18,73 @@ class SendMediaWeb {
     Completer<Map<String, dynamic>> completer =
         Completer<Map<String, dynamic>>();
 
-    Uint8List bytes = await file.readAsBytes();
+    var upChunkUpload = UpChunk(
+      endPoint:
+          Uri.decodeFull(
+            Uri(path: url, queryParameters: parameters).toString(),
+          ).toString(),
+      headers: headers!,
+      file: file,
+      onProgress: (progress) {
+        print('Upload progress: ${progress.ceil()}%');
 
-    html.File htmlFile = html.File(
-      [bytes.toJS].toJS,
-      file.name,
-      html.FilePropertyBag(type: file.mimeType ?? 'application/octet-stream'),
-    );
-
-    Uri uri = Uri.parse(url);
-    uri = uri.replace(queryParameters: parameters);
-
-    LargeFileUploader().upload(
-      method: 'upload',
-      uploadUrl: uri.toString(),
-      data: {"file": htmlFile},
-      headers: headers,
-      onSendProgress: (progress) => streamProgress?.add(progress),
-      onComplete: (response) {
-        if (completer.isCompleted) return;
-
-        try {
-          if (response is String) {
-            Map<String, dynamic> data = jsonDecode(response);
-            completer.complete({"data": data});
-            return;
-          }
-
-          Map<String, dynamic> mapResponseLinkedMap =
-              response.cast<String, dynamic>();
-          completer.complete({"data": mapResponseLinkedMap});
-        } catch (e) {
-          completer.complete({'error': 'Erro ao processar resposta: $e'});
-        }
+        streamProgress?.add(progress.toInt());
       },
-      onFailure: () {
-        if (completer.isCompleted) return;
-        completer.complete({'error': 'Upload falhou'});
+      onError: (String message, int chunk, int attempts) {
+        print('UpChunk error 💥 🙀:');
+        print(' - Message: $message');
+        print(' - Chunk: $chunk');
+        print(' - Attempts: $attempts');
       },
-      cancelFunction: (onCancel) {
-        cancelToken?.whenCancel.then((value) {
-          onCancel();
-        });
+      onSuccess: () {
+        print('Upload complete! 👋');
       },
     );
+
+    // Uint8List bytes = await file.readAsBytes();
+    //
+    // html.File htmlFile = html.File(
+    //   [bytes.toJS].toJS,
+    //   file.name,
+    //   html.FilePropertyBag(type: file.mimeType ?? 'application/octet-stream'),
+    // );
+    //
+    // Uri uri = Uri.parse(url);
+    // uri = uri.replace(queryParameters: parameters);
+    //
+    // LargeFileUploader().upload(
+    //   method: 'upload',
+    //   uploadUrl: uri.toString(),
+    //   data: {"file": htmlFile},
+    //   headers: headers,
+    //   onSendProgress: (progress) => streamProgress?.add(progress),
+    //   onComplete: (response) {
+    //     if (completer.isCompleted) return;
+    //
+    //     try {
+    //       if (response is String) {
+    //         Map<String, dynamic> data = jsonDecode(response);
+    //         completer.complete({"data": data});
+    //         return;
+    //       }
+    //
+    //       Map<String, dynamic> mapResponseLinkedMap =
+    //           response.cast<String, dynamic>();
+    //       completer.complete({"data": mapResponseLinkedMap});
+    //     } catch (e) {
+    //       completer.complete({'error': 'Erro ao processar resposta: $e'});
+    //     }
+    //   },
+    //   onFailure: () {
+    //     if (completer.isCompleted) return;
+    //     completer.complete({'error': 'Upload falhou'});
+    //   },
+    //   cancelFunction: (onCancel) {
+    //     cancelToken?.whenCancel.then((value) {
+    //       onCancel();
+    //     });
+    //   },
+    // );
     return await completer.future;
   }
 }
