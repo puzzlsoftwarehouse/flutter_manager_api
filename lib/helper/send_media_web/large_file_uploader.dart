7f40db63763eefcd3022a99983dc07ee367f376c
DexterZP <andredexter@live.com>
2025-11-24 19:01:14 -0300

    ...

--- lib/helper/send_media_web/large_file_uploader.dart
+++ lib/helper/send_media_web/large_file_uploader.dart
@@ -3,6 +3,8 @@ import 'dart:js_interop';
 import 'package:flutter/foundation.dart';
 import 'package:web/web.dart' as web;
 
+import 'upload_worker_code.dart';
+
 typedef UploadProgressListener = void Function(int progress);
 typedef UploadFailureListener = void Function(String? error);
 typedef UploadCompleteListener = void Function(String response);
@@ -20,7 +22,7 @@ class LargeFileUploader {
       requestId = DateTime.now().millisecondsSinceEpoch.toString();
     }
 
-    final String workerCode = _getWorkerCode();
+    final String workerCode = uploadWorkerCode;
     final JSArray<JSAny> blobParts = [workerCode.toJS].toJS;
     final web.BlobPropertyBag blobOptions =
         web.BlobPropertyBag(type: 'application/javascript');
@@ -132,107 +134,5 @@ class LargeFileUploader {
     } catch (e) {
       onComplete?.call(dataString);
     }
-  }
-
-  String _getWorkerCode() {
-    return '''
-var activeRequests = {};
-
-self.addEventListener('message', async (event) => {
-  var method = event.data.method;
-  var uploadUrl = event.data.uploadUrl;
-  var data = event.data.data;
-  var headers = event.data.headers;
-
-  if (method === 'abort') {
-    var requestId = event.data.requestId;
-    var xhr = activeRequests[requestId];
-    if (xhr) {
-      xhr.abort();
-      delete activeRequests[requestId];
-    }
-    return;
-  }
-  
-  var xhr = uploadFile(method, uploadUrl, data, headers);
-  activeRequests[event.data.requestId] = xhr;
-});
-
-function uploadFile(method, uploadUrl, data, headers) {
-  var xhr = new XMLHttpRequest();
-  var formData = new FormData();
-  var uploadPercent;
-  
-  setData(formData, data);
-
-  xhr.upload.addEventListener('progress', function (d) {
-    if (d.lengthComputable) {
-      uploadPercent = Math.floor((d.loaded / d.total) * 100);
-      postMessage(uploadPercent);
-    }
-  }, false);
-
-  xhr.onload = function () {
-    if (xhr.status >= 200 && xhr.status < 300) {
-      try {
-        var response = xhr.responseText;
-        if (response) {
-          try {
-            var jsonResponse = JSON.parse(response);
-            postMessage(JSON.stringify({"data": jsonResponse}));
-          } catch (e) {
-            postMessage(response);
-          }
-        } else {
-          postMessage(JSON.stringify({"data": {}}));
-        }
-      } catch (e) {
-        postMessage(xhr.responseText || "request completed");
-      }
-    } else {
-      try {
-        var errorResponse = xhr.responseText;
-        if (errorResponse) {
-          try {
-            var jsonError = JSON.parse(errorResponse);
-            postMessage(JSON.stringify({"error": jsonError}));
-          } catch (e) {
-            postMessage(JSON.stringify({"error": errorResponse}));
-          }
-        } else {
-          postMessage(JSON.stringify({"error": "Request failed with status " + xhr.status}));
-        }
-      } catch (e) {
-        postMessage(JSON.stringify({"error": "Request failed with status " + xhr.status}));
-      }
-    }
-  };
-
-  xhr.onerror = function () {
-    postMessage("request failed");
-  };
-
-  xhr.open(method, uploadUrl, true);
-  setHeaders(xhr, headers);
-  xhr.send(formData);
-  return xhr;
-}
-
-function setData(formData, data) {
-  for (let key in data) {
-    formData.append(key, data[key]);
-  }
-}
-
-function setHeaders(xhr, headers) {
-  if (headers) {
-    for (let key in headers) {
-      if (headers.hasOwnProperty(key)) {
-        xhr.setRequestHeader(key, headers[key]);
-      }
-    }
-  }
-}
-''';
   }
 }
